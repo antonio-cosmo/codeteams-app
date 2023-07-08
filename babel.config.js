fd1757b4e9a5bf8136da7e1100ba16558d43231d
Antônio Cosmo <antoniocosmosilvaneto@gmail.com>
2023-07-08 09:13:42 -0300

    first commit

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,23 @@
-module.exports = function(api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-  };
+module.exports = function (api) {
+    api.cache(true);
+    return {
+        presets: ['babel-preset-expo'],
+        plugins: [
+            [
+                'module-resolver',
+                {
+                    root: ['./src'],
+                    extensions: ['.ts', '.tsx', '.js', '.json'],
+                    alias: {
+                        '@assets': './src/assets',
+                        '@components': './src/components',
+                        '@routes': './src/routes',
+                        '@screens': './src/screens',
+                        '@themes': './src/themes',
+                        '@utils': './src/utils',
+                    }
+                }
+            ]
+        ]
+    };
 };
