c695d9d91a4cd04f152266262e6fc3ef8f8c7c28
Antônio Cosmo <antoniocosmosilvaneto@gmail.com>
2023-07-13 14:49:10 -0300

    feat: Component GroupCard

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,14 +1,25 @@
 import { Header } from '@components/Header';
 import { Container, Title } from './styles';
 import { Highlight } from '@components/Highlight';
+import { FlatList } from 'react-native';
+import { useState } from 'react';
+import { GroupCard } from '@components/GroupCard';
 
 export function Groups() {
+    const [groups, setGroups] = useState<string[]>(["Galera da facul", "Galera da levva"])
     return (
         <Container>
             <Header />
             <Highlight
                 title="Turmas"
                 subtitle="jogue com a sua turma"
+            />
+            <FlatList
+                data={groups}
+                keyExtractor={(item) => item}
+                renderItem={({ item }) => (
+                    <GroupCard title={item} />
+                )}
             />
         </Container>
     );
