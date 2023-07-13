029d2d42e1e30fc2a4dd5fc2cef1f98a5862fc98
Antônio Cosmo <antoniocosmosilvaneto@gmail.com>
2023-07-13 14:36:44 -0300

    feat: Component Header

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from "@expo-google-fonts/roboto"
-import { ActivityIndicator, StatusBar } from "react-native";
+import { StatusBar } from "react-native";
 import { Groups } from "@screens/Groups";
 import { ThemeProvider } from "styled-components/native"
 import defaultTheme from "./src/themes";
@@ -8,7 +8,7 @@ export default function App() {
     const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
     return (
         <ThemeProvider theme={defaultTheme}>
-            <StatusBar barStyle="default" />
+            <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
             {fontsLoaded ? <Groups /> : <Loading />}
         </ThemeProvider>
     );
