5c58dd1274a4d1eeb4860e0e6ee7e98d77c2687a
Antônio Cosmo <antoniocosmosilvaneto@gmail.com>
2023-07-13 16:41:56 -0300

    feat: Component Button

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -5,6 +5,7 @@ import { FlatList } from 'react-native';
 import { useState } from 'react';
 import { GroupCard } from '@components/GroupCard';
 import { ListEmpty } from '@components/ListEmpty';
+import { Button } from '@components/Button';
 
 export function Groups() {
     const [groups, setGroups] = useState<string[]>(["Galera da facul", "Galera da levva"])
@@ -26,6 +27,7 @@ export function Groups() {
                     <ListEmpty message='Que tal cadastrar a primeira turma?' />
                 )}
             />
+            <Button title='Criar nova turma' />
         </Container>
     );
 }
