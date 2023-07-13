8177518a65845fd9c7cafbb51f8fb9de229e70a2
Antônio Cosmo <antoniocosmosilvaneto@gmail.com>
2023-07-13 15:05:55 -0300

    feat: Component ListEmpty

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -4,6 +4,7 @@ import { Highlight } from '@components/Highlight';
 import { FlatList } from 'react-native';
 import { useState } from 'react';
 import { GroupCard } from '@components/GroupCard';
+import { ListEmpty } from '@components/ListEmpty';
 
 export function Groups() {
     const [groups, setGroups] = useState<string[]>(["Galera da facul", "Galera da levva"])
@@ -19,6 +20,10 @@ export function Groups() {
                 keyExtractor={(item) => item}
                 renderItem={({ item }) => (
                     <GroupCard title={item} />
+                )}
+                contentContainerStyle={groups.length === 0 && { flex: 1 }}
+                ListEmptyComponent={() => (
+                    <ListEmpty message='Que tal cadastrar a primeira turma?' />
                 )}
             />
         </Container>
