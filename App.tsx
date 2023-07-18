d6d32b4f12d0431be3c010ebc5fd32781e9e1c0b
Antônio Cosmo <antoniocosmosilvaneto@gmail.com>
2023-07-18 13:31:53 -0300

    feat: implements screen NewGroup

--- App.tsx
+++ App.tsx
@@ -4,12 +4,14 @@ import { Groups } from "@screens/Groups";
 import { ThemeProvider } from "styled-components/native"
 import defaultTheme from "./src/themes";
 import { Loading } from "@components/Loading";
+import { NewGroup } from "@screens/NewGroup";
 export default function App() {
     const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
     return (
         <ThemeProvider theme={defaultTheme}>
             <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
-            {fontsLoaded ? <Groups /> : <Loading />}
+            {/* {fontsLoaded ? <Groups /> : <Loading />} */}
+            {fontsLoaded ? <NewGroup /> : <Loading />}
         </ThemeProvider>
     );
 }
